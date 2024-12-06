siddharthsky/CustTermux

tcc: update to 0.9.28 & simpler build.sh

packages/tcc/build.sh
@@ -1,20 +1,23 @@
+_COMMIT=105d70f7b4ce7706652853ece2e35ea8c774005f
+_COMMIT_DATE=20240205
+_COMMIT_SHA256=052d23edee1d946e7e9c5eab2b2489a74e48885860ba1cf7efdcf91cb603c875
+
 TERMUX_PKG_HOMEPAGE=https://bellard.org/tcc/
 TERMUX_PKG_DESCRIPTION="Tiny C Compiler"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-_COMMIT=6a24b762d3e1086dcffd002c68cb5ca3a33a5c6d
-_COMMIT_DATE=20230415
-TERMUX_PKG_VERSION=1:0.9.27-p${_COMMIT_DATE}
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=1:0.9.28-p${_COMMIT_DATE}
 TERMUX_PKG_SRCURL=git+https://repo.or.cz/tinycc.git
-TERMUX_PKG_SHA256=467792219d0172f594ec71bcd6bac9dbb25308cbe9f708bab455b717148b491b
+TERMUX_PKG_SHA256=$_COMMIT_SHA256
 TERMUX_PKG_GIT_BRANCH=mob
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_NO_STATICSPLIT=true
+TERMUX_PKG_DEPENDS="ndk-sysroot"
 
 termux_step_post_get_source() {
 	git fetch --unshallow
-	git checkout $_COMMIT
+	# tcc likes to have a real branch name in its version string
+	git reset --hard $_COMMIT
 
 	local pdate="p$(git log -1 --format=%cs | sed 's/-//g')"
 	if [[ "$TERMUX_PKG_VERSION" != *"${pdate}" ]]; then
@@ -30,81 +33,26 @@ termux_step_post_get_source() {
 	fi
 }
 
-termux_step_pre_configure() {
-	cd $TERMUX_PKG_BUILDDIR
-	rm -rf _bin
-	mkdir _bin
-	cd _bin
-	local _ar="$TERMUX_HOST_PLATFORM-ar"
-	cat > "${_ar}" <<-EOF
-		#!$(command -v sh)
-		exec $(command -v $AR) "\$@"
-	EOF
-	chmod 0700 "${_ar}"
-	export PATH="$(pwd):$PATH"
-}
-
 termux_step_configure() {
-	unset CFLAGS CXXFLAGS
-
-	if [ "${TERMUX_ARCH}" = "arm" ] || [ "${TERMUX_ARCH}" = "i686" ]; then
-		ELF_INTERPRETER_PATH="/system/bin/linker"
-		ANDROID_LIB_PATH="/system/lib:/system/vendor/lib"
-	else
-		ELF_INTERPRETER_PATH="/system/bin/linker64"
-		ANDROID_LIB_PATH="/system/lib64:/system/vendor/lib64"
-	fi
+	# override default step
+        echo "running on : $(uname -a)"
 }
 
 termux_step_make() {
 	(
-		sysinc=
-		otherinc=
-		for d in $(echo | $CC -E -x c - -v 2>&1 | \
-				sed -n '/^#include <...> search/,/^End/p' | \
-				grep '^\s'); do
-			p="$(readlink -f "${d}"):"
-			if [[ "${d}" = */sysroot/usr/* ]]; then
-				sysinc+="${p}"
-			else
-				otherinc+="${p}"
-			fi
-		done
-		sysinc="${sysinc}${otherinc%:}"
 		unset CC CFLAGS LDFLAGS
-		./configure \
-			--prefix="/tmp/tcc.host" \
-			--cpu="${TERMUX_ARCH}" \
-			--sysincludepaths="${sysinc}"
-		make -j $TERMUX_MAKE_PROCESSES tcc
-		mv -f tcc tcc.host
-		make distclean
+		# make a cross-tcc and use it to compile the runtime
+		./configure --targetos="Termux" --cpu="$TERMUX_ARCH" \
+			--sysroot="${TERMUX_STANDALONE_TOOLCHAIN}/sysroot/usr"
+		make -j $TERMUX_MAKE_PROCESSES tcc libtcc1.a
 	)
 
-	./configure \
-		--prefix="$TERMUX_PREFIX" \
-		--cross-prefix="${CC//clang}" \
-		--cc="clang" \
-		--cpu="$TERMUX_ARCH" \
-		--disable-rpath \
-		--elfinterp="$ELF_INTERPRETER_PATH" \
-		--crtprefix="$TERMUX_PREFIX/lib/tcc/crt" \
-		--sysincludepaths="$TERMUX_PREFIX/include/$TERMUX_HOST_PLATFORM:$TERMUX_PREFIX/include:$TERMUX_PREFIX/lib/tcc/include" \
-		--libpaths="$TERMUX_PREFIX/lib:$TERMUX_PREFIX/lib/tcc:$ANDROID_LIB_PATH"
-
-	mv tcc.host tcc
-	touch -d "next minute" tcc
-	make -j ${TERMUX_MAKE_PROCESSES} libtcc1.a
-
-	rm -f tcc
+	# now cross-compile tcc for target
+	./configure --targetos=Termux --cpu="$TERMUX_ARCH"
 	make -j ${TERMUX_MAKE_PROCESSES} tcc
+	make doc
 }
 
-termux_step_post_make_install() {
-	mkdir -p "${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}"/lib/tcc/crt
-	for file in crtbegin_dynamic.o crtbegin_so.o crtend_android.o crtend_so.o; do
-		install -Dm600 \
-			"${TERMUX_STANDALONE_TOOLCHAIN}/sysroot/usr/lib/$TERMUX_HOST_PLATFORM/$TERMUX_PKG_API_LEVEL/$file" \
-			"${TERMUX_PREFIX}/lib/tcc/crt/$file"
-	done
+termux_step_make_install() {
+	make install
 }

be2020/termux-packages

packages/tcc/build.sh
@@ -1,20 +1,24 @@
+_COMMIT=105d70f7b4ce7706652853ece2e35ea8c774005f
+_COMMIT_DATE=20240205
+_COMMIT_SHA256=052d23edee1d946e7e9c5eab2b2489a74e48885860ba1cf7efdcf91cb603c875
+
 TERMUX_PKG_HOMEPAGE=https://bellard.org/tcc/
 TERMUX_PKG_DESCRIPTION="Tiny C Compiler"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-_COMMIT=6a24b762d3e1086dcffd002c68cb5ca3a33a5c6d
-_COMMIT_DATE=20230415
-TERMUX_PKG_VERSION=1:0.9.27-p${_COMMIT_DATE}
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=1:0.9.28-p${_COMMIT_DATE}
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=git+https://repo.or.cz/tinycc.git
-TERMUX_PKG_SHA256=467792219d0172f594ec71bcd6bac9dbb25308cbe9f708bab455b717148b491b
+TERMUX_PKG_SHA256=$_COMMIT_SHA256
 TERMUX_PKG_GIT_BRANCH=mob
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_NO_STATICSPLIT=true
+TERMUX_PKG_DEPENDS="ndk-sysroot"
 
 termux_step_post_get_source() {
 	git fetch --unshallow
-	git checkout $_COMMIT
+	# tcc likes to have a real branch name in its version string
+	git reset --hard $_COMMIT
 
 	local pdate="p$(git log -1 --format=%cs | sed 's/-//g')"
 	if [[ "$TERMUX_PKG_VERSION" != *"${pdate}" ]]; then
@@ -30,81 +34,26 @@ termux_step_post_get_source() {
 	fi
 }
 
-termux_step_pre_configure() {
-	cd $TERMUX_PKG_BUILDDIR
-	rm -rf _bin
-	mkdir _bin
-	cd _bin
-	local _ar="$TERMUX_HOST_PLATFORM-ar"
-	cat > "${_ar}" <<-EOF
-		#!$(command -v sh)
-		exec $(command -v $AR) "\$@"
-	EOF
-	chmod 0700 "${_ar}"
-	export PATH="$(pwd):$PATH"
-}
-
 termux_step_configure() {
-	unset CFLAGS CXXFLAGS
-
-	if [ "${TERMUX_ARCH}" = "arm" ] || [ "${TERMUX_ARCH}" = "i686" ]; then
-		ELF_INTERPRETER_PATH="/system/bin/linker"
-		ANDROID_LIB_PATH="/system/lib:/system/vendor/lib"
-	else
-		ELF_INTERPRETER_PATH="/system/bin/linker64"
-		ANDROID_LIB_PATH="/system/lib64:/system/vendor/lib64"
-	fi
+	# override default step
+        echo "running on : $(uname -a)"
 }
 
 termux_step_make() {
 	(
-		sysinc=
-		otherinc=
-		for d in $(echo | $CC -E -x c - -v 2>&1 | \
-				sed -n '/^#include <...> search/,/^End/p' | \
-				grep '^\s'); do
-			p="$(readlink -f "${d}"):"
-			if [[ "${d}" = */sysroot/usr/* ]]; then
-				sysinc+="${p}"
-			else
-				otherinc+="${p}"
-			fi
-		done
-		sysinc="${sysinc}${otherinc%:}"
 		unset CC CFLAGS LDFLAGS
-		./configure \
-			--prefix="/tmp/tcc.host" \
-			--cpu="${TERMUX_ARCH}" \
-			--sysincludepaths="${sysinc}"
-		make -j $TERMUX_PKG_MAKE_PROCESSES tcc
-		mv -f tcc tcc.host
-		make distclean
+		# make a cross-tcc and use it to compile the runtime
+		./configure --targetos="Termux" --cpu="$TERMUX_ARCH" \
+			--sysroot="${TERMUX_STANDALONE_TOOLCHAIN}/sysroot/usr"
+		make -j $TERMUX_PKG_MAKE_PROCESSES tcc libtcc1.a
 	)
 
-	./configure \
-		--prefix="$TERMUX_PREFIX" \
-		--cross-prefix="${CC//clang}" \
-		--cc="clang" \
-		--cpu="$TERMUX_ARCH" \
-		--disable-rpath \
-		--elfinterp="$ELF_INTERPRETER_PATH" \
-		--crtprefix="$TERMUX_PREFIX/lib/tcc/crt" \
-		--sysincludepaths="$TERMUX_PREFIX/include/$TERMUX_HOST_PLATFORM:$TERMUX_PREFIX/include:$TERMUX_PREFIX/lib/tcc/include" \
-		--libpaths="$TERMUX_PREFIX/lib:$TERMUX_PREFIX/lib/tcc:$ANDROID_LIB_PATH"
-
-	mv tcc.host tcc
-	touch -d "next minute" tcc
-	make -j ${TERMUX_PKG_MAKE_PROCESSES} libtcc1.a
-
-	rm -f tcc
-	make -j ${TERMUX_PKG_MAKE_PROCESSES} tcc
+	# now cross-compile tcc for target
+	./configure --targetos=Termux --cpu="$TERMUX_ARCH"
+	make -j $TERMUX_PKG_MAKE_PROCESSES tcc
+	make doc
 }
 
-termux_step_post_make_install() {
-	mkdir -p "${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}"/lib/tcc/crt
-	for file in crtbegin_dynamic.o crtbegin_so.o crtend_android.o crtend_so.o; do
-		install -Dm600 \
-			"${TERMUX_STANDALONE_TOOLCHAIN}/sysroot/usr/lib/$TERMUX_HOST_PLATFORM/$TERMUX_PKG_API_LEVEL/$file" \
-			"${TERMUX_PREFIX}/lib/tcc/crt/$file"
-	done
+termux_step_make_install() {
+	make install
 }